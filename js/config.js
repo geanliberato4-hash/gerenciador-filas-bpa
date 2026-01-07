fbcd29cbe70e09e97d2fd3e89fe502f7cc711334
geanliberato4-hash <gean.liberato.4@gmail.com>
2026-01-07 12:47:07 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -9,12 +9,9 @@ const firebaseConfig = {
   appId: "1:158845933372:web:491771e52e732bb2277039"
 };
 
-// Inicializa Firebase (proteção contra duplicar)
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
 
-// 🔴 EXPORTA GLOBALMENTE
 window.db = firebase.database();
 window.UNIDADE = "BPA";
-
